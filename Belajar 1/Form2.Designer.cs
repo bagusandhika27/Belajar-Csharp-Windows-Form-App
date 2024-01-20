3f49377972d9169273555c6b63ccf1485731acd2
BAGUSANDHIKA <BAGUSANDHIKA@DESKTOP-CU3K3P9>
2024-01-20 11:05:47 +0700

    Add project files.

---
+++ Belajar 1/Form2.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Belajar_1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 139);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(867, 238);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(130, 46);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(401, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(547, 46);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(663, 46);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 4;
+            textBox4.KeyPress += textBox4_KeyPress;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 93);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Simpan";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(102, 93);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Ubah";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(196, 93);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 8;
+            button3.Text = "Hapus";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Kode Barang";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(130, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Nama Barang";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(547, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Satuan";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(663, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Harga";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(779, 46);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 5;
+            textBox5.KeyPress += textBox5_KeyPress;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(779, 28);
+            label5.Name = "label5";
+            label5.Size = new Size(30, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Stok";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(891, 389);
+            Controls.Add(label5);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Name = "Form2";
+            Text = "Inventori";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+    }
+}
---
+++ Belajar 1/Form2.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace Belajar_1
+{
+    public partial class Form2 : Form
+    {
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+
+        Koneksi Konn = new Koneksi();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        void loadTable()
+        {
+            SqlConnection conn = Konn.GetConn();
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand("SELECT * FROM barang", conn);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "barang");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "barang";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                //dataGridView1.RowHeadersVisible = false;
+                //dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                foreach (DataGridViewRow r in dataGridView1.Rows)
+                {
+                    dataGridView1.Rows[r.Index].HeaderCell.Value = (r.Index + 1).ToString();
+                }
+                dataGridView1.Columns[3].DefaultCellStyle.Format = "C2";
+                dataGridView1.Columns[3].DefaultCellStyle.FormatProvider = CultureInfo.GetCultureInfo("id-ID");
+
+            }
+            catch (Exception G)
+            {
+                MessageBox.Show(G.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        void autoID()
+        {
+            try
+            {
+                SqlConnection conn = Konn.GetConn();
+                cmd = new SqlCommand("SELECT MAX(kodebarang) FROM barang", conn);
+                conn.Open();
+                var currentID = cmd.ExecuteScalar() as string;
+
+                if (currentID == null)
+                {
+                    textBox1.Text = "KD001";
+                }
+                else
+                {
+                    int intval = int.Parse(currentID.Substring(2, 3));
+                    intval++;
+                    textBox1.Text = String.Format("KD{0:000}", intval);
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.ToString());
+            }
+        }
+
+        void clearText()
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            loadTable();
+            clearText();
+            autoID();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.CurrentRow.Selected = true;
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells["kodebarang"].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["namabarang"].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells["satuan"].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells["harga"].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[e.RowIndex].Cells["stok"].Value.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "")
+            {
+                MessageBox.Show("Data Belum Lengkap!");
+            }
+            else
+            {
+                SqlConnection conn = Konn.GetConn();
+                try
+                {
+                    cmd = new SqlCommand("INSERT INTO barang VALUES ('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "', " + textBox4.Text + ", " + textBox5.Text + ")", conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data Berhasil Ditambah!");
+                    loadTable();
+                    clearText();
+                    autoID();
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "")
+            {
+                MessageBox.Show("Data Belum Lengkap!");
+            }
+            else
+            {
+                SqlConnection conn = Konn.GetConn();
+                cmd = new SqlCommand("SELECT * FROM barang WHERE kodebarang = '" + textBox1.Text + "'", conn);
+                conn.Open();
+                SqlDataReader read = cmd.ExecuteReader();
+                try
+                {
+                    if (read.Read())
+                    {
+                        DialogResult dr = MessageBox.Show("Data " + textBox1.Text + " Akan Diubah.\n\nData Sebelumnya\nNama Barang = " + read["namabarang"].ToString() + "\nSatuan = " + read["satuan"].ToString() + "\nHarga = " + read["harga"].ToString() + "\nStok = " + read["stok"].ToString() + "\n\nData Saat ini\nNama Barang = " + textBox2.Text + "\nSatuan = " + textBox3.Text + "\nHarga = " + textBox4.Text + "\nStok = " + textBox5.Text + "\n\nIngin Melanjutkan?", "Konfirmasi", MessageBoxButtons.YesNo);
+                        if (dr == DialogResult.Yes)
+                        {
+                            cmd = new SqlCommand("UPDATE barang SET namabarang = '" + textBox2.Text + "', satuan = '" + textBox3.Text + "', harga = " + textBox4.Text + ", stok = " + textBox5.Text + " WHERE kodebarang = '" + textBox1.Text + "'", conn);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Data Berhasil Diubah!");
+                            conn.Close();
+                            loadTable();
+                            clearText();
+                            autoID();
+                        }
+                        else
+                        {
+                        }
+                    }
+                    else
+                    {
+                    }
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "")
+            {
+                MessageBox.Show("Data Belum Lengkap!");
+            }
+            else
+            {
+                SqlConnection conn = Konn.GetConn();
+                try
+                {
+                    DialogResult dr = MessageBox.Show("Data " + textBox1.Text + " Akan Dihapus.\n\nIngin Melanjutkan?", "Konfirmasi",MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.Yes)
+                    {
+                        cmd = new SqlCommand("DELETE FROM barang WHERE kodebarang = '" + textBox1.Text + "'", conn);
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data Berhasil Dihapus!");
+                        loadTable();
+                        clearText();
+                        autoID();
+                    }
+                    else
+                    {
+
+                    }                    
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                }
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+        }
+    }
+}
